006faa701dbb246c0ac90f4111de794b70f7e080
Kaspter Ju <kaspter@kmatt.net>
2017-02-15 10:04:15 +0800

    fixed envsetup

--- envsetup.sh
+++ envsetup.sh
@@ -3,8 +3,8 @@
 
 
 
-OPENCV_PKG_PATH=./../libopencv/lib/pkgconfig
-OPENCV_LIB_PATH=./../libopencv/lib
+OPENCV_PKG_PATH=./../libocv/lib/pkgconfig
+OPENCV_LIB_PATH=./../libocv/lib
 
 export PKG_CONFIG_PATH=$OPENCV_PKG_PATH
 export LD_LIBRARY_PATH=$OPENCV_LIB_PATH
